6c5d644cd48761fd2a99a2791330863ef34c5e6c
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-06-17 15:05:29 -0500

    We Have Cubegit add -A Using a local version of three.js for now, will figure out how to properly import later.

--- public/src/RePuG.js
+++ public/src/RePuG.js
@@ -1,6 +1,3 @@
-//importing entire three.js core lib
-import * as THREE from "three";
-
 //Initialize a new scene for our RPG Battle
 const battle = new THREE.Scene();
 
@@ -15,4 +12,25 @@ const camera = new THREE.PerspectiveCamera(
 //initialize a renderer to render our scene with the camera
 const renderer = new THREE.WebGLRenderer(); //WebGL is goto renderer
 renderer.setSize(window.innerWidth, window.innerHeight); //sets dimmensions for render
-document.body.appendChild(rederer.domElement); //add renderer to HTML
+document.body.appendChild(renderer.domElement); //add renderer to HTML
+
+//Create a cube to render
+const geometry = new THREE.BoxGeometry(); //Geometry set to box
+const material = new THREE.MeshBasicMaterial({ color: 0x381648 }); //Mesh Material set to basic color Russian Violet
+const hyperionCube = new THREE.Mesh(geometry, material); //Create a mesh that applies the mesh material to the geometry
+battle.add(hyperionCube); //add our new mesh obj to the scene
+
+camera.position.z = 3; //move the camera out
+
+//Render(Animate) Loop
+function animate() {
+  requestAnimationFrame(animate);
+  //rotate the cube
+  hyperionCube.rotation.x += 0.01;
+  hyperionCube.rotation.y += 0.01;
+
+  renderer.render(battle, camera);
+}
+
+//begin the infinite recursion!!
+animate();
